2c060da637f61907edfee3883ecf2288606a94c3
OlgaLyalyueva <olgalyalyueva@gmail.com>
2021-01-27 14:48:24 +0200

    Add read only fields for ArticleSerializer and AuthorSerializer

--- apprestapi/serializers.py
+++ apprestapi/serializers.py
@@ -5,10 +5,11 @@ from .models import Article, Author
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('title', 'description', 'body', 'author')
-
+        fields = ('id', 'title', 'description', 'body', 'author')
+        read_only_fields = ('id',)
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ('name', 'email')
+        fields = ('id', 'name', 'email')
+        read_only_fields = ('id',)
